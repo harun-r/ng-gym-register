c1999e64c9601f010e3dd6c57ba45153a78981ac
Harun <mdharunbd88@gmail.com>
2023-04-24 22:39:56 +0600

    reg init

--- src/app/mat/mat.module.ts
+++ src/app/mat/mat.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
   exports: [
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class MatModule { }
